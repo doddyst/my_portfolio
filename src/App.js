16bab68dfb3025ef78c4ad489dd81c2cb0512568
doddyst <dodiscan12@gmail.com>
2024-04-21 07:05:54 +0800

    portfolio About Done

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/header';
 import Home from './components/home/home';
+import About from './components/about/about';
 
 function App() {
   return (
     <>
     <Header />
+    
     <main className='main'>    
     <Home />
+    <About />
     </main>
-
     </>
   );
 }
---
+++ src/components/about/info.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div className='about_info grid'>
+        <div className='about_box'>
+        <i class="uil uil-award-alt about_icon"></i>
+            <h3 className='about_title' >Experience</h3>
+            <span className='about_subtitle'> Fresh Graduate</span>
+        </div>
+
+        <div className='about_box'>
+        <i class="uil uil-briefcase-alt about_icon"></i>
+            <h3 className='about_title' >Completed</h3>
+            <span className='about_subtitle'> Few Project</span>
+        </div>
+
+        <div className='about_box'>
+        <i class="uil uil-headphones about_icon"></i>
+            <h3 className='about_title' >asd</h3>
+            <span className='about_subtitle'>adsda</span>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Info
